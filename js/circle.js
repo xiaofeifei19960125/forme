2cc4b3d9baab01c44c7d90aae7fc9c822e869a43
xiaofeifei19960125 <1197383929@qq.com>
2017-11-21 09:30:54 +0800

    自我介绍

---
+++ js/circle.js
@@ -0,0 +1,130 @@
+// 圆形进度条
+$(function(){
+
+	$('#html').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj1 = $('#html').data('radialIndicator');
+			radialObj1.animate(80);
+
+			$('#css').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj2 = $('#css').data('radialIndicator');
+			radialObj2.animate(80);
+			$('#javascript').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj3 = $('#javascript').data('radialIndicator');
+			radialObj3.animate(70);
+			$('#jquery').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj4 = $('#jquery').data('radialIndicator');
+			radialObj4.animate(70);
+			$('#bootstrap').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj5 = $('#bootstrap').data('radialIndicator');
+			radialObj5.animate(70);
+			$('#java').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj6 = $('#java').data('radialIndicator');
+			radialObj6.animate(70);
+			$('#javaee').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj7 = $('#javaee').data('radialIndicator');
+			radialObj7.animate(85);
+			$('#spring').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj8 = $('#spring').data('radialIndicator');
+			radialObj8.animate(90);
+			
+			$('#springmvc').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj9 = $('#springmvc').data('radialIndicator');
+			radialObj9.animate(90);
+		
+				$('#mybatis').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj10 = $('#mybatis').data('radialIndicator');
+			radialObj10.animate(90);
+			
+			$('#oracle').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj11 = $('#oracle').data('radialIndicator');
+			radialObj11.animate(80);
+			
+			$('#mysql').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj12 = $('#mysql').data('radialIndicator');
+			radialObj12.animate(90);
+	
+})
